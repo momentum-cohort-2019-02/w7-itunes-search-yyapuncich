2ac2c4f7a9d2debf524221696a75b403978e3666
Yifaa Yapuncich <yifaasy@gmail.com>
2019-03-29 13:29:06 -0400

    json data showing up on page

--- main.js
+++ main.js
@@ -11,26 +11,31 @@
 
 
 document.addEventListener('DOMContentLoaded', function () {
-    
-    let searchField = document.querySelector('#music-search')
-    searchField.addEventListener('change', function (event) {
-            event.preventDefault()
 
-            let searchText = 'term='
-            searchText += encodeURIComponent(searchField.value)
-            console.log(searchText)
-            searchField.value = ''
-
-            const promise = fetch(
-                    `https://itunes-api-proxy.glitch.me/search?${searchText}`
-                )
-                .then(function (response) {
-                    if (!response.ok) {
-                        throw Error(response.statusText)
-                    }
-                    // console.log(response.json())
-                    return response.json()
-                })
-            return promise
+            let searchField = document.querySelector('#music-search')
+            searchField.addEventListener('change', function (event) {
+                event.preventDefault()
+                // allows custimization of the 'change' event
+                // define searchtext, encode the text and add it to searchtext, 
+                // clear the entry so we can search again:
+                let searchText = 'term='
+                searchText += encodeURIComponent(searchField.value)
+                console.log(searchText)
+                searchField.value = ''
+                // add searchtext to api search and return the json data
+                fetch(`https://itunes-api-proxy.glitch.me/search?${searchText}`, {
+                        method: 'POST',
+                        searchText: searchText
+                    })
+                    .then(function (response) {
+                        if (!response.ok) {
+                            throw Error(response.statusText)
+                        }
+                        return response.text()
+                    })
+                    .then(text => {
+                        const searchResult = document.createRange().createContextualFragment(text)
+                        document.querySelector('.search-result-item').appendChild(searchResult)
+                    })
+            })
         })
-    })
