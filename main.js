c5dc388e4f359fdea6e08e8a2a7dbf4b4ae73a56
Yifaa Yapuncich <yifaasy@gmail.com>
2019-03-28 22:48:54 -0400

    search term is being genrated, nothing is being done with it yet

--- main.js
+++ main.js
@@ -1,33 +1,36 @@
 // let apiUrl = "https://itunes-api-proxy.glitch.me/"
 /* globals fetch */
 
-let searchTerm = document.querySelector('#music-search')
-console.log(searchTerm)
 
 // add event listener for sumbit search form, prevent default
 
 // defines response to searching the iTunes API. Returns the json data from search term
-function getMusicSearch (searchTerm) {
-    const promise = fetch(
-        `https://itunes-api-proxy.glitch.me/search?term=${searchTerm}`
-    )
-    .then(function (response) {
-        if (!response.ok) {
-            throw Error(response.statusText)
-        }
-        // console.log(response.json())
-        return response.json()
-    })
-    return promise
-}
+
 
 // data attribute with url of song needed to pLAY on PLAY button(click)
 
 
 document.addEventListener('DOMContentLoaded', function () {
-    searchTerm.addEventListener('input', function () {
-        console.log(searchTerm.value)
-    })
-    getMusicSearch(searchTerm)
+    
+    let searchField = document.querySelector('#music-search')
+    searchField.addEventListener('change', function (event) {
+            event.preventDefault()
 
-})
+            let searchText = 'term='
+            searchText += encodeURIComponent(searchField.value)
+            console.log(searchText)
+            searchField.value = ''
+
+            const promise = fetch(
+                    `https://itunes-api-proxy.glitch.me/search?${searchText}`
+                )
+                .then(function (response) {
+                    if (!response.ok) {
+                        throw Error(response.statusText)
+                    }
+                    // console.log(response.json())
+                    return response.json()
+                })
+            return promise
+        })
+    })
