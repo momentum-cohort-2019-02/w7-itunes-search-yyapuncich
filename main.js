006736ec44f4a279920ab233e1ba5d0d66a9f7a4
Yifaa Yapuncich <yifaasy@gmail.com>
2019-03-29 21:07:40 -0400

    reorganized my code- still same spot as before in terms of function

--- main.js
+++ main.js
@@ -1,41 +1,42 @@
 // let apiUrl = "https://itunes-api-proxy.glitch.me/"
 /* globals fetch */
+function query(selector) {
+    return document.querySelector(selector)
+}
 
+function queryAll(selector) {
+    return document.querySelectorAll(selector)
+}
+// restructuring my javascript file to look more like Abbys... using more functions
+function getArtistData(searchText) {
+    console.log('get artist data')
 
-// add event listener for sumbit search form, prevent default
-
-// defines response to searching the iTunes API. Returns the json data from search term
-
-
-// data attribute with url of song needed to pLAY on PLAY button(click)
-
+    return fetch(`https://itunes-api-proxy.glitch.me/search?${searchText}`)
+        .then(response => {
+            if (!response.ok) {
+                console.log('not ok')
+                throw Error(response.statusText)
+            }
+            console.log('responjson')
+            return response.json()
+        })
+}
 
 document.addEventListener('DOMContentLoaded', function () {
+    let searchField = query('#music-search')
+    searchField.addEventListener('change', event => {
+        event.preventDefault()
+        let searchText = 'term='
+        searchText += encodeURIComponent(searchField.value)
+        console.log(searchText)
+        searchField.value = ''
 
-            let searchField = document.querySelector('#music-search')
-            searchField.addEventListener('change', function (event) {
-                event.preventDefault()
-                // allows custimization of the 'change' event
-                // define searchtext, encode the text and add it to searchtext, 
-                // clear the entry so we can search again:
-                let searchText = 'term='
-                searchText += encodeURIComponent(searchField.value)
-                console.log(searchText)
-                searchField.value = ''
-                // add searchtext to api search and return the json data
-                fetch(`https://itunes-api-proxy.glitch.me/search?${searchText}`, {
-                        method: 'POST',
-                        searchText: searchText
-                    })
-                    .then(function (response) {
-                        if (!response.ok) {
-                            throw Error(response.statusText)
-                        }
-                        return response.text()
-                    })
-                    .then(text => {
-                        const searchResult = document.createRange().createContextualFragment(text)
-                        document.querySelector('.search-result-item').appendChild(searchResult)
-                    })
+        console.log('dom content loaded')
+        getArtistData(searchText)
+            .then(musicData => {
+                console.log(musicData)
+
+
             })
-        })
+    })
+})
